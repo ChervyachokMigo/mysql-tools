57943b9f3dea03f93234454ed847f6b4b354564f
ChervyachokMigo <github@svdgod.ru>
2024-06-24 19:28:15 +0500

    autoupdate

--- src/backups/export_table_csv.ts
+++ src/backups/export_table_csv.ts
@@ -70,7 +70,7 @@ export const export_table_csv = async ( csv_params: CSV_PARAMS ) => {
 
 		const action = Array.isArray(tablename) ? tablename[0] : tablename;
 
-		if (!action || find_model(action)) {
+		if (!action || !find_model(action)) {
 			console.error('tablename invalid', action);
 			return { 
 				error: 'tablename invalid', 
